c0f664ff8c095581590cdecfca50e5f8e183e85c
Ved <vedjain773@gmail.com>
2024-06-18 20:27:25 +0530

    second

--- main.py
+++ main.py
@@ -5,8 +5,49 @@ root.title("Super Tic-Tac-Toe")
 
 alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
 buttons = {}
+claimed = {}
 
 isXturn = True
+
+def disableGrid(alphabet):
+    for i in range (0, 9):
+        id = f"{alphabet}{i}"
+        buttons[id].config(state = tk.DISABLED)
+        buttons[id].config(bg="#808080")
+
+def aBT(id: str):
+    return buttons[id].cget("text")
+
+def checkGrid(alpha):
+    rows = [0, 3, 6]
+    columns = [0, 1, 2]
+    for row in rows:
+        if checkThree(alpha, row, "X", True):
+            claimed[f"{alpha}"] = "X"
+            disableGrid(alpha)
+        elif checkThree(alpha, row, "O", True):
+            claimed[f"{alpha}"] = "O"
+            disableGrid(alpha)
+
+    for col in columns:
+        if checkThree(alpha, col, "X", False):
+            claimed[f"{alpha}"] = "X"
+            disableGrid(alpha)
+        elif checkThree(alpha, col, "O", False):
+            claimed[f"{alpha}"] = "O"
+            disableGrid(alpha)
+
+
+def checkAll():
+    for i in alphabets:
+        checkGrid(i)
+    print(claimed)
+
+def checkThree(alphabet, num, letter, row: bool):
+    if (row):
+        return aBT(f"{alphabet}{num}") == aBT(f"{alphabet}{num + 1}") == aBT(f"{alphabet}{num + 2}") == letter
+    else:
+        return aBT(f"{alphabet}{num}") == aBT(f"{alphabet}{num + 3}") == aBT(f"{alphabet}{num + 6}") == letter
 
 def toggle(): 
     global isXturn
@@ -16,7 +57,7 @@ def disableOthers(id: str):
     index = int(id[1])
     alpha = alphabets[index]
     for i in buttons:
-        if not i[0] == alpha:
+        if not i[0] == alpha or alpha in claimed:
             buttons[i].config(state = tk.DISABLED)
             buttons[i].config(bg="#808080")
         else:
@@ -28,17 +69,19 @@ def changeText(id: str):
     if (isXturn):
         buttons[id].config(text = "X")
         toggle()
+        checkAll()
         disableOthers(id=id)
     else:
-        buttons[id].config(text = "0")
+        buttons[id].config(text = "O")
         toggle()
+        checkAll()
         disableOthers(id=id)
 
 def createGrid(alphabet, columnStart, rowStart):
     rowCounter = rowStart
     columnCounter = columnStart
     for i in range(0, 9):
-        button = tk.Button(root, text=" ", height=4, width=8, command=lambda sid=f"{alphabet}{i}": changeText(sid))
+        button = tk.Button(root, text=" ", height=1, width=3, font=("Helvetica", 30), command=lambda sid=f"{alphabet}{i}": changeText(sid))
         button.grid(row=rowCounter, column=columnCounter)
         buttons[f"{alphabet}{i}"] = button
         columnCounter = columnCounter + 1
